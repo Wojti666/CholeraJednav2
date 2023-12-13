d8ef934d13481704adb64229763297cfff658737
Wojti <wojcieszko666@o2.pl>
2023-12-13 18:24:04 +0100

    burger added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 const footerYear = document.querySelector('.footer__year')
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
 handleCurrentYear()
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+	navMobile.classList.toggle('active')
+	document.body.classList.toggle('sticky-body')
+}
+
+navBtn.addEventListener('click', handleNav)
